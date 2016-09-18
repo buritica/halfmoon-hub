c0da5a29fdbfab4c5e87fc2617ff2046eb395bdf
Juan Pablo Buritica <buritica@gmail.com>
2016-09-18 17:12:30 -0400

    [rename] doubletail to halfmoon

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'doubletail-hub',
+    modulePrefix: 'halfmoon-hub',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
