28a5ad1b8f00b0fc53f1c483f3ca552ea9c38536
Juan Pablo Buriticá <buritica@gmail.com>
2023-05-20 13:39:50 +0200

    [update] better handling of urls

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import Head from 'next/head'
-import Typography from 'typography'
-import theme from 'typography-theme-grand-view'
-import { TypographyStyle, GoogleFont } from 'react-typography'
-import { Container, Grid } from 'react-responsive-grid'
+import Head from "next/head";
+import Typography from "typography";
+import theme from "typography-theme-grand-view";
+import { TypographyStyle, GoogleFont } from "react-typography";
+import { Container, Grid } from "react-responsive-grid";
 
-const typography = new Typography(theme)
-const { rhythm } = typography
+const typography = new Typography(theme);
+const { rhythm } = typography;
 
 export default () => (
   <div>
     <Head>
       <title>📺 Televisora Nacional 📺</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
       <TypographyStyle typography={typography} />
       <GoogleFont typography={typography} />
     </Head>
@@ -20,17 +23,31 @@ export default () => (
       <Grid columns={12}>
         <h1>📺 Televisora Nacional 📺</h1>
         <h3>Media Managers</h3>
-        <p><a href='http://tv.buriti.ca:8989/' target='_blank'>📺 TV Series </a></p>
-        {/* <p><a href='http://tv.buriti.ca:8484/' target='_blank'>🎥 Movies (couchpotato - user:buritica/pass:buritica)</a></p> */}
-        <p><a href='http://tv.buriti.ca:8585/' target='_blank'>🎥 Movies </a></p>
+        <p>
+          <a href="//halfmoon.local:8989/" target="_blank">
+            📺 TV Series{" "}
+          </a>
+        </p>
+        <p>
+          <a href="//halfmoon.local:8585/" target="_blank">
+            🎥 Movies{" "}
+          </a>
+        </p>
 
         <h3>Media Downloaders</h3>
-        <p><a href='http://tv.buriti.ca:8080/' target='_blank'>⬇️ SABnzbd+</a></p>
+        <p>
+          <a href="//halfmoon.local:8080/" target="_blank">
+            ⬇️ SABnzbd+
+          </a>
+        </p>
 
         <h3>Media Servers</h3>
-        <p><a href='http://tv.buriti.ca:32400/' target='_blank'>💻 Plex Media Server</a></p>
-
+        <p>
+          <a href="//halfmoon.local:32400/" target="_blank">
+            💻 Plex Media Server
+          </a>
+        </p>
       </Grid>
     </Container>
   </div>
-)
+);
